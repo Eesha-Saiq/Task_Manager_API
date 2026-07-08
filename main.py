e8d6b326ccf364ba0e634cb7b2722da16904fd8d
Eesha Saiq <eeshasaiq722@gmail.com>
2026-07-08 15:04:14 +0500

    Add authorization, session management and CORS support

--- main.py
+++ main.py
@@ -1,38 +1,88 @@
-from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException, Header, Depends, Request
 from pydantic import BaseModel, Field
 from typing import Optional
+from dotenv import load_dotenv
+from starlette.middleware.sessions import SessionMiddleware
 import uuid
+import os
+
+load_dotenv()
+
+API_TOKEN = os.getenv("API_TOKEN")
+print("API TOKEN =", API_TOKEN)
 
 app = FastAPI()
 
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="my_session_secret"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 tasks = []
+
+def verify_token(
+    request: Request,
+    authorization: str = Header(None)
+):
+    if authorization != f"Bearer {API_TOKEN}":
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized"
+        )
+
+    request.session["authenticated"] = True
 
 class TaskCreate(BaseModel):
     title: str = Field(..., min_length=3)
     description: Optional[str] = None
     completed: bool = False
 
+
 class TaskUpdate(BaseModel):
     title: str = Field(..., min_length=3)
     description: Optional[str] = None
     completed: bool = False
 
+
 class Task(BaseModel):
-    id: str 
-    title: str = Field(..., min_length=3)
+    id: str
+    title: str
     description: Optional[str] = None
     completed: bool = False
+
 
 @app.get("/")
 def home():
     return {"message": "Task Manager API Running"}
 
+
 @app.get("/tasks")
-def get_tasks():
+def get_tasks(token: str = Depends(verify_token)):
     return tasks
 
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: str, token: str = Depends(verify_token)):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
 @app.post("/tasks")
-def create_task(task: TaskCreate):
+def create_task(task: TaskCreate, token: str = Depends(verify_token)):
 
     new_task = Task(
         id=str(uuid.uuid4()),
@@ -44,21 +94,13 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return {
-        "message": "Task added",
+        "message": "Task added successfully",
         "task": new_task
     }
 
-@app.get("/tasks/{task_id}")
-def get_task(task_id: int):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-
-    raise HTTPException(status_code=404, detail="Task not found")
-
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: str, updated_task: TaskUpdate):
+def update_task(task_id: str, updated_task: TaskUpdate, token: str = Depends(verify_token)):
 
     for index, task in enumerate(tasks):
 
@@ -83,8 +125,9 @@ def update_task(task_id: str, updated_task: TaskUpdate):
         detail="Task not found"
     )
 
+
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
+def delete_task(task_id: str, token: str = Depends(verify_token)):
 
     for index, task in enumerate(tasks):
 
@@ -96,4 +139,32 @@ def delete_task(task_id: int):
                 "message": "Task deleted successfully",
                 "task": deleted_task
             }
-    raise HTTPException(status_code=404, detail="Task not found")
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
+@app.get("/me")
+def get_me(request: Request):
+
+    if "authenticated" not in request.session:
+        raise HTTPException(
+            status_code=401,
+            detail="No active session"
+        )
+
+    return {
+        "message": "Session Active",
+        "session": request.session
+    }
+
+@app.post("/logout")
+def logout(request: Request):
+
+    request.session.clear()
+
+    return {
+        "message": "Logged out successfully"
+    }
